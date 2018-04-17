082a6600c228ef7c1eecad21b485d9964aff9397
Harrison Healey <harrisonmhealey@gmail.com>
2018-04-16 21:10:58 -0400

    Added additional Scryfall types

--- src/client/scryfall/types.ts
+++ src/client/scryfall/types.ts
@@ -1,3 +1,67 @@
+export enum BorderColor {
+    Black = 'black',
+    Borderless = 'borderless',
+    Gold = 'gold',
+    Silver = 'silver',
+    White = 'white'
+};
+
+export interface Card {
+    id: string,
+    oracle_id: string,
+    // multiverse_ids: string[],
+    // mtgo_id: number,
+    // mtgo_foil_id: number,
+    // uri: string,
+    // scryfall_uri: string,
+    // prints_search_uri: string,
+    // rulings_uri: string,
+
+    name: string,
+    // layout: CardLayout,
+    cmc: number,
+    type_line: string,
+    oracle_text: string,
+    mana_cost: string,
+    power: string,
+    toughness: string,
+    loyalty: string,
+    // life_modifier: string,
+    // hand_modifier: string,
+    colors: Color[],
+    color_indicator?: Color[],
+    // all_parts: object[],
+    card_faces?: CardFace[],
+    // legalities: object,
+    // foil: boolean,
+    // nonfoil: boolean,
+    // oversized: boolean,
+    // edhrec_rank: number,
+
+    set: string,
+    set_name: string,
+    collector_number: string,
+    set_search_uri: string,
+    scryfall_set_uri: string,
+    image_uris: ImageUris,
+    highres_image: boolean,
+    reprint: boolean,
+    digital: boolean,
+    rarity: Rarity,
+    flavor_text: string,
+    artist: string,
+    illustration_id: string,
+    frame: string,
+    full_art: boolean,
+    watermark: string,
+    border_color: BorderColor,
+    story_spotlight_number: number,
+    story_spotlight_uri: string,
+    timeshifted: boolean,
+    colorshifted: boolean,
+    futureshifted: boolean,
+};
+
 export enum Color {
     White = 'W',
     Blue = 'U',
@@ -6,18 +70,19 @@ export enum Color {
     Green = 'G'
 };
 
-export interface Card {
-    id: string,
-    oracle_id: string,
-
+export interface CardFace {
     name: string,
-    cmc: number,
     type_line: string,
+    oracle_text: string,
+    mana_cost: string,
     colors: Color[],
-
-    image_uris: {
-        [type: string]: string
-    }
+    color_indicator: Color[],
+    power: string,
+    toughness: string,
+    loyalty: string,
+    flavor_text: string,
+    illustration_id: string,
+    image_uris: ImageUris
 };
 
 export interface Error {
@@ -26,4 +91,22 @@ export interface Error {
     details: string,
     type?: string,
     warnings?: string[]
+};
+
+export enum ImageType {
+    Png = 'png',
+    BorderCrop = 'border_crop',
+    ArtCrop = 'art_crop',
+    Large = 'large',
+    Normal = 'normal',
+    Small = 'small'
+};
+
+export type ImageUris = {[type in ImageType]: string};
+
+export enum Rarity {
+    Common = 'common',
+    Uncommon = 'uncommon',
+    Rare = 'rare',
+    MythicRare = 'mythic',
 };
