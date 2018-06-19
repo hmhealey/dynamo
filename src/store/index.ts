023d534242584fb42753fbe19d086d61f43d4322
Harrison Healey <harrisonmhealey@gmail.com>
2018-06-19 19:42:17 -0400

    Fixed being unable to turn on module.hot

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,12 +9,12 @@ export default function configureStore() {
         applyMiddleware(thunk)
     );
 
-    // if (module.hot) {
-    //     // Enable Webpack hot module replacement for reducers
-    //     module.hot.accept('../reducers', () => {
-    //       store.replaceReducer(rootReducer)
-    //     })
-    // }
+    if (module.hot) {
+        // Enable Webpack hot module replacement for reducers
+        module.hot.accept('../reducers', () => {
+            store.replaceReducer(rootReducer)
+        });
+    }
 
     return store;
 }
